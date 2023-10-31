dbbe32b34340e12c01c470b9812523db3a93f660
younglee0280 <younglee0280@gmail.com>
2023-10-31 21:49:14 +0900

    10/31

--- 04/function.cpp
+++ 04/function.cpp
@@ -1,3 +1,5 @@
+#include    <iostream>
+using namespace std;
 int main()
 {
     double a[50], b[100];
@@ -5,7 +7,7 @@ int main()
     double max = a[0];
     for(int i=1; i < 50; i++)
         if(max < a[i])
-        max = a[i];
+            max = a[i];
     cout << max << endl;
     max = b[0];
     for(int i=1; i < 100; i++)
