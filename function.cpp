2c433ac40488e1234576055288d81eaf503b1f50
younglee0280 <younglee0280@gmail.com>
2023-10-31 21:26:37 +0900

    10/31

---
+++ function.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    double a[50], b[100];
+
+    double max = a[0];
+    for(int i=1; i < 50; i++)
+        if(max < a[i])
+        max = a[i];
+    cout << max << endl;
+    max = b[0];
+    for(int i=1; i < 100; i++)
+        if(max < b[i])
+            max = b[i];
+    cout << max << endl;
+    }
